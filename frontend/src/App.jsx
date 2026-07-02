c69dea2ddad416cd82995e81a2af3e3c52caa5a9
Shriyansh Pandey <Shriyansh@7021639629@gmail.com>
2026-07-02 08:33:12 +0530

    Day 2 - Resume upload functionality completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,99 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
 
-  const [message, setMessage] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [result, setResult] = useState(null);
 
-  useEffect(() => {
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
 
-    fetch("http://localhost:5001/api/resume")
-      .then(res => res.json())
-      .then(data => {
-        setMessage(data.message);
-      });
+    const handleUpload = async () => {
 
-  }, []);
+    if (!selectedFile) {
+        alert("Please select a resume first.");
+        return;
+    }
 
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Smart Resume Analyzer</h1>
+    const formData = new FormData();
 
-      <h2>{message}</h2>
-    </div>
-  );
+    formData.append("resume", selectedFile);
+
+    try {
+
+        const response = await axios.post(
+            "http://localhost:5001/api/resume/upload",
+            formData
+        );
+
+        setResult(response.data);
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+    };
+
+    return (
+        <div style={{
+            padding: "40px",
+            textAlign: "center"
+        }}>
+
+            <h1>Smart Resume Analyzer</h1>
+
+            <br />
+
+            <input
+                type="file"
+                accept=".pdf,.doc,.docx"
+                onChange={handleFileChange}
+            />
+
+            <br /><br />
+
+            <p>
+                {selectedFile
+                    ? selectedFile.name
+                    : "No file selected"}
+            </p>
+
+            <button onClick={handleUpload}>
+                Upload Resume
+            </button>
+
+            {result && (
+                <div style={{ marginTop: "30px" }}>
+                  <h3>Upload Result</h3>
+
+                  <p>
+                    <strong>Status:</strong> {result.message}
+                  </p>
+
+                  <p>
+                    <strong>Original Name:</strong> {result.file.originalName}
+                  </p>
+
+                  <p>
+                    <strong>Saved Name:</strong> {result.file.savedName}
+                  </p>
+
+                  <p>
+                    <strong>File Size:</strong> {result.file.size} bytes
+                  </p>
+
+                  <p>
+                    <strong>Type:</strong> {result.file.type}
+                  </p>
+                </div>
+            )}
+
+        </div>
+    );
 }
 
 export default App;
